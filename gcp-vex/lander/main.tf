b236ebc00d75553e6aa34e0616428245f578c75a
Claude Bamberger <claude.bamberger@sogeti.com>
2023-06-01 12:59:34 +0200

    montage disque si existe, ubuntu, clean

--- gcp-vex/lander/main.tf
+++ gcp-vex/lander/main.tf
@@ -12,6 +12,12 @@ provider "google" {
   //zone    = ""
 }
 data "google_compute_zones" "available" {
+
+}
+data "google_compute_disk" "wopr_data" {
+  name    = "wopr-data"
+  project = var.GCP_PROJECT_ID
+  zone    = data.google_compute_zones.available.names[0]
 }
 resource "google_compute_network" "lander" {
   name                    = "lander"
@@ -28,14 +34,16 @@ resource "google_compute_network" "lander-subnet" {
   name                    = "lander-test-subnet"
   auto_create_subnetworks = false
 }
-
 resource "google_compute_instance" "wopr" {
   description  = "wopr"
   name         = "wopr"
   machine_type = "e2-micro"
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      #image = "debian-cloud/debian-11"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      #ubuntu-os-cloud/ubuntu-2204-lts
+      #https://gmusumeci.medium.com/how-to-deploy-an-ubuntu-linux-vm-instance-in-gcp-using-terraform-b94d0ed3a3a4
     }
   }
   network_interface {
@@ -44,4 +52,10 @@ resource "google_compute_instance" "wopr" {
     }
   }
   zone = data.google_compute_zones.available.names[0]
+}
+
+resource "google_compute_attached_disk" "wopr_data" {
+  count    = (data.google_compute_disk.wopr_data.id == null) ? 0 : 1
+  disk     = data.google_compute_disk.wopr_data.id
+  instance = google_compute_instance.wopr.id
 }
