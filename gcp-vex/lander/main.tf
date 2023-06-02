eb8373da349c89793b3816b0d09b3960360e1ad0
Claude Bamberger <claude.bamberger@sogeti.com>
2023-06-02 18:16:41 +0200

    finalisation firewall, ssh et user ansible

--- gcp-vex/lander/main.tf
+++ gcp-vex/lander/main.tf
@@ -43,6 +43,44 @@ resource "google_compute_subnetwork" "lander-subnet-private" {
     ip_cidr_range = "172.16.8.0/28"
   }
 }
+resource "google_compute_address" "admin-public-ip" {
+  name       = "admin-public-ip"
+  project    = var.GCP_PROJECT_ID
+  region     = var.GCP_REGION
+  depends_on = [google_compute_firewall.landline]
+}
+resource "google_compute_firewall" "landline" {
+  # https://www.middlewareinventory.com/blog/create-linux-vm-in-gcp-with-terraform-remote-exec/#compute_firewall_block_-_Allow_SSH_and_HTTPS_connections
+  name        = "landline"
+  description = "firewall for ssh access"
+  network     = google_compute_network.lander.name
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  destination_ranges = ["${var.GCP_LANDER_SUBNET_PUBLIC}"]
+
+  direction = "INGRESS"
+  log_config {
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+  #source_ranges      = ["${var.GCP_SECURE_CIDR}"]
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh-admin"]
+}
+resource "google_compute_firewall" "landlineInternal" {
+  name        = "landline-internal"
+  description = "firewall for ssh access"
+  network     = google_compute_network.lander.name
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  destination_ranges = ["${var.GCP_LANDER_SUBNET_PRIVATE}"]
+  source_ranges      = ["${var.GCP_LANDER_SUBNET_PUBLIC}"]
+  target_tags        = ["ssh-internal"]
+}
+
 resource "google_compute_instance" "woprPub" {
   description  = "wopr-pub"
   name         = "wopr-pub"
@@ -57,8 +95,29 @@ resource "google_compute_instance" "woprPub" {
   }
   network_interface {
     subnetwork = google_compute_subnetwork.lander-subnet-public.id
-    access_config { /* Ephemeral public IP */ }
+    access_config {
+      nat_ip = google_compute_address.admin-public-ip.address
+    }
   }
+  metadata = {
+    # NB: it's either oslogin OR ssh
+    enable-oslogin = false
+    ssh-keys       = "ansible:${file(var.GCP_PUBLIC_KEY_PATH)}\nmex:${file(var.GCP_PUBLIC_KEY_PATH)}"
+  }
+  provisioner "remote-exec" {
+    connection {
+      # user is added to sudoers (good)
+      host        = google_compute_address.admin-public-ip
+      user        = "ansible"
+      private_key = file(var.GCP_PRIVATE_KEY_PATH)
+      timeout     = "30s"
+    }
+    inline = [
+      "sudo apt-get update",
+    ]
+  }
+  tags       = ["ssh-admin", "ssh-internal"]
+  depends_on = [google_compute_firewall.landline, google_compute_firewall.landlineInternal]
 }
 resource "google_compute_instance" "woprPriv" {
   description  = "wopr-priv"
@@ -73,9 +132,22 @@ resource "google_compute_instance" "woprPriv" {
   network_interface {
     subnetwork = google_compute_subnetwork.lander-subnet-private.id
   }
+  tags       = ["ssh-internal"]
+  depends_on = [google_compute_firewall.landlineInternal]
 }
 resource "google_compute_attached_disk" "wopr_data" {
-  count    = (data.google_compute_disk.wopr_data.id == null) ? 0 : 1
-  disk     = data.google_compute_disk.wopr_data.id
-  instance = google_compute_instance.woprPriv.id
+  count       = (data.google_compute_disk.wopr_data.id == null) ? 0 : 1
+  disk        = data.google_compute_disk.wopr_data.id
+  instance    = google_compute_instance.woprPriv.id
+  project     = var.GCP_PROJECT_ID
+  mode        = "READ_WRITE"
+  device_name = "wopr-data-0"
+  zone        = data.google_compute_disk.wopr_data.zone
+}
+
+output "woprPub" {
+  value = google_compute_instance.woprPub.hostname
+}
+output "publicAdminIp" {
+  value = google_compute_address.admin-public-ip.address
 }
