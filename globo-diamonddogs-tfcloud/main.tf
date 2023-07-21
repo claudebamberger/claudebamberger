c39548b919a4f8d19effde93ac082c74990bfebd
Claude Bamberger <claude.bamberger@sogeti.com>
2023-07-21 18:58:50 +0200

    test sentinelle

--- globo-diamonddogs-tfcloud/main.tf
+++ globo-diamonddogs-tfcloud/main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "diamond_dogs_admin" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["92.0.0.0/8"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
