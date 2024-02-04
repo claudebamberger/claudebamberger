d8551e8a42a78005bac840070508e7ad29407493
claudebbg <me@claudebbg.com>
2024-02-04 09:27:05 +0100

    correction declaration deplacee

--- aws-vex/land/modules/wopr/providers.tf
+++ aws-vex/land/modules/wopr/providers.tf
@@ -1,6 +1,7 @@
 terraform {
+  # version tf
+  required_version = ">=1.7, <2.0"
   required_providers {
-    required_version = ">=1.7, <2.0"
     aws = {
       source  = "hashicorp/aws"
       version = ">=5.0, <6.0"
